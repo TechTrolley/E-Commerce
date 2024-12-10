533932f016a8d4a2db12990d9ca660fcf090c3ea
TechTrolley <techtrolleyproject@gmail.com>
2024-12-10 20:21:01 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Login from "./login.jsx"
+import Hello from "./Hello.jsx"
+
+function App() {
+  
+
+  return (
+    <>
+      <Login/>
+      <Hello/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Hello.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './index.css';
+
+function Hello() {
+  return (
+    <div className='hello'>Hello</div>
+  )
+}
+
+export default Hello
