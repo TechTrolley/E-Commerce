a489e2225b52f4dc390f45f5bfcc082d382a5f2a
selvakumarpro <22us36@anjaconline.org>
2025-02-06 12:31:04 +0530

    Add files via upload

--- src/Components/phonesList.jsx
+++ src/Components/phonesList.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import watchImage from './s222.webp';
+import watchImage from './samsungs22.jfif';
 import phoneImage from './s21.png';
-import lapImage from './samsungs23fe.png';
+import lapImage from './samsungs23fe.jpg';
 import Controllers from './samsungs24ultra.webp';
-import Mouse from './mouse.webp';
-import Drones from './drone.webp';
-import Rtx from './rtx.jpg';
-import speak from './speak.webp';
+import Mouse from './iphone15.png';
+import Drones from './iphone14.png';
+import Rtx from './iphone16.png';
+import speak from './iphone16promax.png';
 
 const phoneList = () => {
   const phoneList = [
@@ -31,32 +31,32 @@ const phoneList = () => {
     },
     {
       id: 4,
-      name: 'Controllers',
-      description: 'Shop the Best Deals on Gaming Controllers – Precision, Comfort, and Style at Your Fingertips!',
+      name: 'Samsung S24 Ultra',
+      description: 'Explore the specs for Samsung Galaxy S24 Ultra, including color availability, display size, camera features, battery life and more...',
       image: Controllers,
     },
     {
       id: 5,
-      name: 'Drones',
-      description: ' Explore Amazing Drone Deals – Capture Stunning Views and Elevate Your Adventures.',
+      name: 'Apple iPhone 14',
+      description: 'iPhone 14 Technical Specifications. The iPhone 14 display has rounded corners that follow a beautiful curved design...',
       image: Drones,
     },
     {
       id: 6,
-      name: 'Mouse',
-      description: 'Unbeatable Deals on Mice – Precision, Style, and Performance at Your Fingertips!',
+      name: 'Apple iphone 15',
+      description: 'INNOVATIVE DESIGN — iPhone 15 features a durable color-infused glass and aluminum design. It is splash and water resistant...',
       image: Mouse,
     },
     {
       id: 7,
-      name: 'Graphics Card',
-      description: 'Unleash Ultimate Performance with Our GPU Deals – Upgrade Today and Dominate Tomorrow!',
+      name: 'Apple iphone 16',
+      description: 'The iPhone 16 features a 6.1" and 6.7" Super Retina XDR display with up to 2,000 nits brightness....',
       image: Rtx,
     },
     {
       id: 8,
-      name: 'Speakers',
-      description: 'Grab the Best Deals on Speakers – Crystal-Clear Sound for Every Moment.',
+      name: 'Apple iphone 16 Pro Max',
+      description: 'The iPhone 16 Pro Max sports a 6.9-inch LTPO Super Retina XDR OLED display and 120Hz refresh rate support...',
       image: speak,
     },
   ];
