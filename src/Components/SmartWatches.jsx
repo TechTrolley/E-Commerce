ee5680f78bc85943c481b86e14ea70856d3a9fd5
selvakumarpro <22us36@anjaconline.org>
2025-02-14 15:57:09 +0530

    Add files via upload

--- src/Components/SmartWatches.jsx
+++ src/Components/SmartWatches.jsx
@@ -1,93 +1,97 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import watchImage from '../assets/samsungswatch2.avif';
-import phoneImage from '../assets/samsungswatch1.jpg';
-import lapImage from '../assets/tuf.png';
-import Controllers from '../assets/joys.jpg';
-import Mouse from '../assets/mouse.webp';
-import Drones from '../assets/drone.webp';
-import Rtx from '../assets/rtx.jpg';
-import speak from '../assets/speak.webp';
+import phoneImage from '../assets/samsunggalaxyfe.jpg';
+import lapImage from '../assets/samsunggalaxywatch4.webp';
+import Controllers from '../assets/samsunggalaxywatchultra.jpeg';
+import Mouse from '../assets/applewatch9.webp';
+import Drones from '../assets/applewatchultra.png';
+import Rtx from '../assets/applewatchultra2.png';
+import speak from '../assets/applewatch10.png';
+import b1 from '../assets/boatwaveelevatepro.png';
+import b2 from '../assets/boatstorm.avif';
+import b3 from '../assets/applewatch10.png';
+import b4 from '../assets/applewatch10.png';
 
 const SmartWatches = () => {
   const phoneList = [
     {
       id: 1,
-      name: 'Samsung Galaxy Fit3',
+      name: 'Samsung Galaxy Watch FE',
       description: 'Explore the latest and greatest smartphones on the market.',
       image: phoneImage,
     },
     {
       id: 2,
-      name: '',
+      name: 'Samsung Galaxy Fit3',
       description: 'Discover the future of wearable technology with our collection of smartwatches.',
       image: watchImage,
     },
     {
       id: 3,
-      name: 'Laptops',
+      name: 'Samsung Galaxy Watch 4',
       description: 'Grab the Best Deals on Laptops with Power, Performance, and Portability at Unbeatable Prices!',
       image: lapImage,
     },
     {
       id: 4,
-      name: 'Controllers',
+      name: 'Samsung Galaxy Ultra',
       description: 'Shop the Best Deals on Gaming Controllers – Precision, Comfort, and Style at Your Fingertips!',
       image: Controllers,
     },
     {
       id: 5,
-      name: 'Drones',
+      name: 'Apple Watch Ultra',
       description: ' Explore Amazing Drone Deals – Capture Stunning Views and Elevate Your Adventures.',
       image: Drones,
     },
     {
       id: 6,
-      name: 'Mouse',
+      name: 'Apple Watch 9',
       description: 'Unbeatable Deals on Mice – Precision, Style, and Performance at Your Fingertips!',
       image: Mouse,
     },
     {
       id: 7,
-      name: 'Graphics Card',
+      name: 'Apple Watch Ultra 2',
       description: 'Unleash Ultimate Performance with Our GPU Deals – Upgrade Today and Dominate Tomorrow!',
       image: Rtx,
     },
     {
       id: 8,
-      name: 'Speakers',
+      name: 'Apple Watch 10',
       description: 'Grab the Best Deals on Speakers – Crystal-Clear Sound for Every Moment.',
       image: speak,
     },
     {
       id: 9,
-      name: 'Speakers',
+      name: 'Boat Wave Elevate Pro',
       description: 'Grab the Best Deals on Speakers – Crystal-Clear Sound for Every Moment.',
-      image: speak,
+      image: b1,
     },
     {
       id: 10,
-      name: 'Speakers',
+      name: 'Boat Storm',
       description: 'Grab the Best Deals on Speakers – Crystal-Clear Sound for Every Moment.',
-      image: speak,
+      image: b2,
     },
     {
       id: 11,
       name: 'Speakers',
       description: 'Grab the Best Deals on Speakers – Crystal-Clear Sound for Every Moment.',
-      image: speak,
+      image: b3,
     },
     {
       id:12,
       name: 'Speakers',
       description: 'Grab the Best Deals on Speakers – Crystal-Clear Sound for Every Moment.',
-      image: speak,
+      image: b4,
     },
   ];
 
   return (
     <div className="max-w-screen-xl mx-auto p-6">
-      <h1 className="text-3xl font-bold text-center mb-8">SmartPhones</h1>
+      <h1 className="text-3xl font-bold text-center mb-8">SmartWatches</h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
         {phoneList.map((product) => {
           const isSmartphone = product.name === 'SmartPhones';
