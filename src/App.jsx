de0e210158334861f319d47cbfca59e25324ed0d
selvakumarpro <22us36@anjaconline.org>
2025-03-01 11:17:50 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,96 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './Components/Navbar'; // Correct path to Navbar component
-import Products from './Components/Products'; // Adjusted path to Products component
+import Navbar from './Components/Navbar';
+import Products from './Components/Products';
 import SmartphonesList from './Components/phonesList';
 import ProductDetails from './Components/ProductDetails';
 import SmartWatches from './Components/SmartWatches';
 import Laptops from './Components/Laptops';
-//import Login from './Components/login';
+import Login from './Components/login';
 import Signup from './Components/signup';
 import WatchDetails from './Components/WatchDetails';
-import Account from "./Components/Accounts";
-
 
 function App() {
   return (
     <Router>
       <Routes>
-        {/* Routes with navbar */}
-        <Route path="/" element={
-          <>
-            <Navbar />
-          </>
-        }/>
-        
-        <Route path="/products" element={
-          
-          <>
-            <Navbar />
-            <Products />
-          </>
-        }/>
+        {/* Home Route with Navbar */}
+        <Route
+          path="/"
+          element={
+            <>
+              <Navbar />
+              <SmartWatches />
+            </>
+          }
+        />
 
-        {/* Route without navbar */}
-        <Route path="/smartphones" element={<SmartphonesList />} />
-        <Route path="/product/:id" element={<ProductDetails />} />
-        <Route path="/smartwatches" element={<SmartWatches />} />
-        <Route path="/laptops" element={<Laptops />} />
-        <Route path="/" element={<SmartWatches />} />
-        <Route path="/watch/:id" element={<WatchDetails />} />
-        <Route path="/account" element={<Account />} />
+        {/* Products Route with Navbar */}
+        <Route
+          path="/products"
+          element={
+            <>
+              <Navbar />
+              <Products />
+            </>
+          }
+        />
 
-        
-        <Route path="/signup" element={<Signup />} />        
+        {/* Smartphones Route with Navbar */}
+        <Route
+          path="/smartphones"
+          element={
+            <>
+              <SmartphonesList />
+            </>
+          }
+        />
+
+        {/* SmartWatches Route with Navbar */}
+        <Route
+          path="/smartwatches"
+          element={
+            <>
+              <SmartWatches />
+            </>
+          }
+        />
+
+        {/* Laptops Route with Navbar */}
+        <Route
+          path="/laptops"
+          element={
+            <>
+              <Laptops />
+            </>
+          }
+        />
+
+        {/* Product Details Route with Navbar */}
+        <Route
+          path="/product/:id"
+          element={
+            <>
+              <ProductDetails />
+            </>
+          }
+        />
+
+        {/* Watch Details Route with Navbar */}
+        <Route
+          path="/watch/:id"
+          element={
+            <>
+              <WatchDetails />
+            </>
+          }
+        />
+
+        {/* Login Route without Navbar */}
+        <Route path="/login" element={<Login />} />
+
+        {/* Signup Route without Navbar */}
+        <Route path="/signup" element={<Signup />} />
       </Routes>
     </Router>
   );
